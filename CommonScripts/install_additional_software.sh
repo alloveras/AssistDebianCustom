9e4f4f2b4c1c0e2d1c895480771ffdac45729950
Albert Lloveras <alloveras@salleurl.edu>
2016-01-12 00:41:29 +0100

    [PULL REQUEST] Modified install_additional_software.sh to ask the user whether to install Java 1.8 SDK or not

--- CommonScripts/install_additional_software.sh
+++ CommonScripts/install_additional_software.sh
@@ -7,12 +7,51 @@ su root -c "apt-get -y --force-yes install libdvdcss2";
 
 arch=`uname -m`;
 
-#Check system architecture
+#Check system architecture to decide which codecs to install
 if [ "$arch" == "x86_64" ]; then
 	su root -c "apt-get -y --force-yes install w64codecs";
 else
 	su root -c "apt-get -y --force-yes install w32codecs";
 fi
+
+#Ask the user if he/she wants to install Java 1.8 SDK
+echo -ne "${BLUE}[INFO]:${NO_COLOR} Would you like to install Java 1.8 SDK? [Y/N]\n";
+
+while read option; do
+
+  if [ "$option" == "Y" ] || [ "$option" == "y" ]; then
+
+    echo -ne "${BLUE}[INFO]:${NO_COLOR} Installing Oracle Sun Java SDK 1.8...\n";
+    if [ "$arch" == "x86_64" ]; then
+      cd /opt/;
+      wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-x64.tar.gz";
+      tar xzf jdk-8u66-linux-x64.tar.gz;
+      rm -rf jdk-8u66-linux-x64.tar.gz;
+    else
+      cd /opt/;
+      wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-i586.tar.gz";
+      tar xzf jdk-8u66-linux-i586.tar.gz;
+      rm -rf jdk-8u66-linux-i586.tar.gz;
+    fi
+
+    update-alternatives --install "/usr/bin/java" "java" "/opt/jdk1.8.0_66/bin/java" 1;
+    update-alternatives --install "/usr/bin/javac" "javac" "/opt/jdk1.8.0_66/bin/javac" 1;
+    update-alternatives --install "/usr/bin/javaws" "javaws" "/opt/jdk1.8.0_66/bin/javaws" 1;
+    update-alternatives --set java /opt/jdk1.8.0_66/bin/java;
+    update-alternatives --set javac /opt/jdk1.8.0_66/bin/javac;
+    update-alternatives --set javaws /opt/jdk1.8.0_66/bin/javaws;
+
+    echo -ne "${BLUE}[INFO]:${GREEN} Oracle Sun Java SDK 1.8 installed!${NO_COLOR}\n";
+
+    break;
+
+  elif [ "$option" == "N" ] || [ "$option" == "n" ]; then
+    break;
+  else
+    echo -ne "${RED}[ERROR]: Incorrect option!${NO_COLOR}  Would you like to install Java 1.8 SDK? [Y/N]\n";
+  fi
+
+done
 
 echo -ne "${BLUE}[INFO]:${GREEN} Common audio & video codecs intalled!${NO_COLOR}\n";
 
